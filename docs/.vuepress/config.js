cc983a67049fc21a675f0e39ad80743e697e73fd
ImCaO <imcao@imcao.cn>
2022-06-20 21:45:52 +0800

    🛠️ 搜索功能升级为 docsearch

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,8 @@
+const { defaultTheme } = require("@vuepress/theme-default");
+const { docsearchPlugin } = require("@vuepress/plugin-docsearch");
+
 module.exports = {
-  lang: "zh-CN",
+  lang: " ",
   title: "Just Laws",
   description: "法律和法律都是相互依存的",
   head: [
@@ -7,7 +10,7 @@ module.exports = {
     ["script", { src: "https://hm.baidu.com/hm.js?f1b6f06a4a48c2db87fcba1a4b3c3ac4" }],
   ],
 
-  themeConfig: {
+  theme: defaultTheme({
     logo: "/images/logo.png",
     navbar: [
       {
@@ -164,21 +167,61 @@ module.exports = {
     contributorsText: "贡献者",
     notFound: ["页面未找到"],
     backToHome: "回到主页",
-    toggleDarkMode: "切换夜间模式",
+    toggleColorMode: "切换夜间模式",
     toggleSidebar: "切换侧边栏",
-  },
+  }),
 
   plugins: [
-    [
-      "@vuepress/plugin-search",
-      {
-        locales: {
-          "/": {
-            placeholder: "搜索",
+    docsearchPlugin({
+      apiKey: "c1b57ecf806bfe5c370d3de23b858065",
+      appId: "M6984MENBN",
+      indexName: "just_laws",
+      searchParameters: {
+        attributesToSnippet: ['lvl1:30', 'content:25']
+      },
+      locales: {
+        "/": {
+          placeholder: "搜索文档",
+          translations: {
+            button: {
+              buttonText: "搜索文档",
+              buttonAriaLabel: "搜索文档",
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: "清除查询条件",
+                resetButtonAriaLabel: "清除查询条件",
+                cancelButtonText: "取消",
+                cancelButtonAriaLabel: "取消",
+              },
+              startScreen: {
+                recentSearchesTitle: "搜索历史",
+                noRecentSearchesText: "没有搜索历史",
+                saveRecentSearchButtonTitle: "保存至搜索历史",
+                removeRecentSearchButtonTitle: "从搜索历史中移除",
+                favoriteSearchesTitle: "收藏",
+                removeFavoriteSearchButtonTitle: "从收藏中移除",
+              },
+              errorScreen: {
+                titleText: "无法获取结果",
+                helpText: "你可能需要检查你的网络连接",
+              },
+              footer: {
+                selectText: "选择",
+                navigateText: "切换",
+                closeText: "关闭",
+                searchByText: "搜索提供者",
+              },
+              noResultsScreen: {
+                noResultsText: "无法找到相关结果",
+                suggestedQueryText: "你可以尝试查询",
+                reportMissingResultsText: "你认为该查询应该有结果？",
+                reportMissingResultsLinkText: "点击反馈",
+              },
+            },
           },
         },
-        maxSuggestions: 10,
       },
-    ],
+    }),
   ],
 };
