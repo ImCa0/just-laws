db95f7a4bdd757867785419069743c1a5a07c19c
ImCaO <imcao@imcao.cn>
2022-08-05 23:39:37 +0800

    🛠️ 格式修正

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -43,9 +43,9 @@ module.exports = {
         text: "民商法",
         children: [
           { text: "民法典", link: "/civil-and-commercial/civil-code/" },
+          { text: "著作权法", link: "/civil-and-commercial/copyright-law/" },
           { text: "消费者权益保护法", link: "/civil-and-commercial/protection-of-the-rights-and-interests-of-consumers/" },
           { text: "公司法", link: "/civil-and-commercial/company-law/" },
-          { text: "著作权法", link: "/civil-and-commercial/copyright-law/" },
         ],
       },
       {
