0f6827bffafb3016aac0fcc8a4e68fbc9b24aca6
ImCaO <imcao@imcao.cn>
2026-01-28 01:27:18 +0800

    📘 收录《人民武装警察法》

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -62,6 +62,7 @@ module.exports = {
           { text: "突发事件应对法", link: "/administrative/emergency-response-law/" },
           { text: "中医药法", link: "/administrative/traditional-chinese-medicine-law/" },
           { text: "人口与计划生育法", link: "/administrative/population-and-family-planning-law/" },
+          { text: "人民武装警察法", link: "/administrative/people-s-armed-police-law/" },
         ],
       },
       {
